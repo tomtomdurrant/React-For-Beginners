2c259eec6f338754ad5b79728ed6bd431c955540
Tom Durrant <tomchdurrant@gmail.com>
2020-08-16 13:50:12 +0100

    Finished video 16 - updating state

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,16 +2,59 @@ import React from "react";
 import Header from "../components/Header";
 import Inventory from "../components/Inventory";
 import Order from "../components/Order";
+import sampleFishes from "../sample-fishes";
+import Fish from "./Fish";
 
 class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {},
+  };
+
+  addFish = (fish) => {
+    // Take a copy of the state
+    const fishes = { ...this.state.fishes };
+    // Add new fish to that new variable
+    fishes[`fish${Date.now()}`] = fish;
+    // Set the new fishes object to state
+    // In es6 if your property and value are the same name
+    // You can just pass in the one name
+    this.setState({ fishes: fishes });
+  };
+
+  loadSampleFishes = () => {
+    this.setState({ fishes: sampleFishes });
+  };
+
+  addToOrder = (key) => {
+    // Take a copy of state
+    const order = { ...this.state.order };
+    // Either add to the order or update the number in our order
+    order[key] = order[key] + 1 || 1;
+    this.setState({ order });
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map((key) => (
+              <Fish
+                key={key}
+                index={key}
+                details={this.state.fishes[key]}
+                addToOrder={this.addToOrder}
+              />
+            ))}
+          </ul>
         </div>
         <Order />
-        <Inventory />
+        <Inventory
+          addFish={this.addFish}
+          loadSampleFishes={this.loadSampleFishes}
+        />
       </div>
     );
   }
--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -1,7 +1,16 @@
 import React, { Component } from "react";
+import AddFishForm from "./AddFishForm";
 
 export default class Inventory extends Component {
   render() {
-    return <div className="inventory">Inventory</div>;
+    return (
+      <div className="inventory">
+        <h2>Inventory</h2>
+        <AddFishForm addFish={this.props.addFish} />
+        <button onClick={this.props.loadSampleFishes}>
+          Load Sample Fishes
+        </button>
+      </div>
+    );
   }
 }
